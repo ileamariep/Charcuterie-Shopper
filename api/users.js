90c2c589f3a6b684516ba07818f076d1bcc60f5a
Jeffery <jefferyhauckjr@gmail.com>
2021-06-23 21:07:32 -0500

    Small Order Methods update and Tables adjustment

--- api/users.js
+++ api/users.js
@@ -6,12 +6,42 @@ const { requireUser } = require("./utils");
 const { JWT_SECRET } = process.env;
 
 usersRouter.post("/register", async (req, res, next) => {
-  //  Create a new user. Require username and password, and hash password before saving user to DB. Require all passwords to be at least 8 characters long.
-  //  Throw errors for duplicate username, or password-too-short.
-  const { username, password } = req.body;
-  try {
-    const _user = await getUserByUsername(username);
-});
+    //  Create a new user. Require username and password, and hash password before saving user to DB. Require all passwords to be at least 8 characters long.
+    //  Throw errors for duplicate username, or password-too-short.
+    const { username, password } = req.body;
+    try {
+      const _user = await getUserByUsername(username);
+      if (_user) {
+        throw Error("Username already exists.");
+      }
+      if (password.length < 8) {
+        throw Error("Password must be at least 8 characters long.");
+      }
+      const user = await createUser({ username, password });
+      if (!user) {
+        throw Error(`Error creating user.`);
+      } else {
+        const token = jwt.sign(
+          {
+            id: user.id,
+            username,
+          },
+          JWT_SECRET,
+          {
+            expiresIn: "1w",
+          }
+        );
+        res.send({
+          message: "You have successfully registered!",
+          user,
+          token,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  });
 
   
 usersRouter.get('/me', requireUser, async (req, res, next) => {
--- db/orders.js
+++ db/orders.js
@@ -16,6 +16,114 @@ async function createOrder({ date_ordered, total_price }) {
 
 }
 
+// async function createOrder(orderList) {
+//     if (orderList.length === 0) {
+//       return;
+//     }
+//     const insertValues = orderList.map((_, index) => `$${index + 1}`).join("), (");
+//     const selectValues = orderList.map((_, index) => `$${index + 1}`).join(", ");
+//     try {
+//       await client.query(
+//         `INSERT INTO orders(id)
+//         VALUES (${insertValues})
+//         ON CONFLICT (id) DO NOTHING;`,
+//         orderList
+//       );
+//       const { rows } = await client.query(
+//         `SELECT * FROM orders
+//         WHERE id
+//         IN (${selectValues});`,
+//         orderList
+//       );
+//       return rows;
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
+
+async function getAllOrders() {
+    try {
+      const { rows: orderId } = await client.query(`
+        SELECT id
+        FROM orders;
+      `);
+  
+      const orders = await Promise.all(orderId.map(
+        order => getOrderById( order.id )
+      ));
+  
+      return orders;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+async function getOrderByUser(userId) {
+    try {
+      const { rows: orderId } = await client.query(`
+        SELECT id 
+        FROM orders 
+        WHERE "usersId"=${ userId };
+      `);
+  
+      const orders = await Promise.all(orderId.map(
+        order => getOrderById( order.id )
+      ));
+  
+      return orders;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+async function getOrderById(orderId) {
+    try {
+      const { rows: [ order ]  } = await client.query(`
+        SELECT *
+        FROM orders
+        WHERE id=$1;
+      `, [orderId]);
+  
+      const { rows: ingredients } = await client.query(`
+      SELECT ingredients.*
+      FROM ingredients
+      JOIN order_ingredients ON ingredients.id=order_ingredients."ingredientId"
+      WHERE order_ingredients."orderId"=$1;
+    `, [orderId])
+
+    const { rows: [user] } = await client.query(`
+      SELECT id, username, name, location
+      FROM users
+      WHERE id=$1;
+    `, [order.usersId])
+
+  
+      order.ingredients = ingredients;
+      order.user = user;
+  
+      delete user.usersId;
+  
+      return order;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+// async function addIngredientToOrder({ routineId, activityId, count, duration }) {
+//     try {
+//       const { rows : [routine_activities] } = await client.query(`
+//         INSERT INTO routine_activities("routineId", "activityId", count, duration) 
+//         VALUES($1, $2, $3, $4) 
+//         RETURNING *;
+//       `, [routineId, activityId, count, duration]);
+  
+//       return routine_activities;
+//     } catch (error) {
+//       throw error;
+//     }
+// }
+
 module.exports = {
     client,
     createOrder,
