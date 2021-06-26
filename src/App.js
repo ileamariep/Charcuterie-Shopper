5b0512543757605faaa7005bf2f3865f9a990673
smcmanis82 <sharon.mcmanis@gmail.com>
2021-06-26 10:40:30 -0500

    SM register and login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
-import { Header } from "./components";
+import React, { useEffect, useState } from "react";
+import { Header, Register, Login } from "./components";
 
-function App() {
+const App = () => {
+  const [auth, setAuth] = useState(false);
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem("token"))) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  });
   return (
     <div className="App">
       <Header />
+      <Register />
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
